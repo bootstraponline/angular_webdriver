8ad96c7c6895502fb41f8e8930a5cd010f51dfef
bootstraponline <code@bootstraponline.com>
2015-05-12 15:25:52 -0400

    Release 0.0.7

--- lib/angular_webdriver/version.rb
+++ lib/angular_webdriver/version.rb
@@ -1,4 +1,4 @@
 module AngularWebdriver
-  VERSION = '0.0.6' unless defined? ::AngularWebdriver::VERSION
-  DATE    = '2015-05-09' unless defined? ::AngularWebdriver::DATE
+  VERSION = '0.0.7' unless defined? ::AngularWebdriver::VERSION
+  DATE    = '2015-05-12' unless defined? ::AngularWebdriver::DATE
 end
