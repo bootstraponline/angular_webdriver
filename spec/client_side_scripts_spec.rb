83fee9d666931155f7f61dab007ac02fde11f9fd
bootstraponline <code@bootstraponline.com>
2015-05-17 15:44:22 -0400

    Fix client_side_scripts_spec

--- spec/client_side_scripts_spec.rb
+++ spec/client_side_scripts_spec.rb
@@ -3,7 +3,7 @@ require_relative 'spec_helper'
 describe 'client side scripts' do
 
   it 'matches the methods and content of clientSideScripts.json' do
-    expected = symbolize_keys JSON.parse File.read File.join __dir__, '../lib/angular_webdriver/protractor/clientSideScripts.json'
+    expected = symbolize_keys JSON.parse File.read File.join __dir__, '../gen/clientSideScripts.json'
     actual   = ClientSideScripts.client_side_scripts
 
     # actual must be identical to expected.
