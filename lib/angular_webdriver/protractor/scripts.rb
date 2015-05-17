036f2323c5788e73621edc475c3c5726ae046045
bootstraponline <code@bootstraponline.com>
2015-05-17 12:34:00 -0400

    Remove old files

--- lib/angular_webdriver/protractor/scripts.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'json'
-require 'ostruct'
-
-client_side_scripts = OpenStruct.new JSON.parse File.read 'clientSideScripts.json'
-
-puts client_side_scripts.waitForAngular
