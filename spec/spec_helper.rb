edd2043d78f93c5063fdeb8a677478b3f93da7cc
bootstraponline <code@bootstraponline.com>
2015-08-26 16:44:32 -0400

    Revert "Try before/after :each to fix test-queue split failures"
    This reverts commit 9aa49aab9d6779ea3d8b194377065a302f0b8f03.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,11 +51,11 @@ RSpec.configure do |config|
   config.backtrace_exclusion_patterns = [osx_rvm_gems, linux_rvm_gems]
 
   # http://www.rubydoc.info/github/rspec/rspec-core/RSpec/Core/Hooks
-  config.before(:each) do
+  config.before(:suite) do
     @spec_helpers = SpecHelpers.instance
   end
 
-  config.after(:each) do
+  config.after(:suite) do
     @spec_helpers.driver_quit rescue nil
   end
 
